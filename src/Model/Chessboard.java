fa67ec93358d31d88af36d45759ff6c09618bdbe
Ronald De Mesa <ronaldreyjr061201@gmail.com>
2025-03-24 00:31:19 +0800

    add initialize for chessboard and add chessSide class

--- src/Model/Chessboard.java
+++ src/Model/Chessboard.java
@@ -3,11 +3,18 @@ package Model;
 import java.util.ArrayList;
 
 public class Chessboard {
-    public Chessboard(){
+    ArrayList<ArrayList<ChessBox>> chessboard;
+
+    public Chessboard() {
         initChessboard();
     }
 
     private void initChessboard() {
-
+        ArrayList<ArrayList<ChessBox>> chessboard = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            ArrayList<ChessBox> row = new ArrayList<>(8);
+            chessboard.add(row);
+        }
+        this.chessboard = chessboard;
     }
 }
